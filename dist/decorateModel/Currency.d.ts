3d5dd0d5a39570652b4aa9861f5a8d4159ead6f6
Ezios <eziosyanse@gmail.com>
2019-04-09 16:15:54 +0800

    remove dist from git

--- dist/decorateModel/Currency.d.ts
+++
@@ -1,9 +0,0 @@
-import { Numeric } from './Numeric';
-export declare class Currency extends Numeric {
-    readonly presentation: string;
-    readonly yuan: number;
-    readonly kind: typeof Currency;
-    static isCurrency<P>(model: P): boolean;
-    format: (formatString?: string | undefined) => string;
-    clone: (value?: number | undefined) => Currency;
-}
